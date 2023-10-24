d023eaabb08cd86c93c8b77bde197268b7f89584
rafaelsmgomes <rafael.smgomes@gmail.com>
2023-10-23 21:01:42 -0600

    Advent of Code until day3

---
+++ day4/src/main.rs
@@ -0,0 +1,51 @@
+use std::ops::RangeInclusive;
+
+use itertools::Itertools;
+
+trait InclusiveRangeExt {
+    fn contains_range(&self, other: &Self) -> bool;
+
+    fn contains_or_is_contained(&self, other: &Self) -> bool {
+        self.contains_range(other) || other.contains_range(self)
+    }
+
+    fn overlaps(&self, other: &Self) -> bool;
+
+    fn overlaps_or_is_overlapped(&self, other: &Self) -> bool {
+        self.overlaps(other) || other.overlaps(self)
+    }
+}
+
+impl<T> InclusiveRangeExt for RangeInclusive<T>
+where
+    T: PartialOrd,
+{
+    fn contains_range(&self, other: &Self) -> bool {
+        self.contains(other.start()) && self.contains(other.end())
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        self.contains(other.start()) || self.contains(other.end())
+    }
+}
+
+fn main() {
+    let redundant = include_str!("input.txt")
+        .lines()
+        .map(|l| {
+            l.split(',')
+                .map(|range| {
+                    range
+                        .split('-')
+                        .map(|n| n.parse().expect("range start/end should be u32"))
+                        .collect_tuple::<(u32, u32)>()
+                        .map(|(start, end)| start..=end)
+                        .expect("each range should have a start and end")
+                })
+                .collect_tuple::<(_, _)>()
+                .expect("each line must have a pair of ranges")
+        })
+        .filter(|(a, b)| a.overlaps_or_is_overlapped(b))
+        .count();
+    dbg!(redundant);
+}
